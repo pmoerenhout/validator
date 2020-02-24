3e35f4d7abc22eb197f2fe02a9610983b5c77ce1
Pim Moerenhout <pim.moerenhout@gmail.com>
2020-02-24 12:24:46 +0100

    Use GSMA EID and Swift IBAN for testing.

--- src/test/java/com/github/pmoerenhout/validator/Iso7064Test.java
+++ src/test/java/com/github/pmoerenhout/validator/Iso7064Test.java
@@ -14,33 +14,43 @@ public class Iso7064Test extends TestCase {
     iso7064 = new Iso7064();
   }
 
-  public void testIsValid() throws Exception {
+  public void testIsValidEid() throws Exception {
     Assert.assertTrue(iso7064.isValid("89001012012341234012345678901224"));
     Assert.assertTrue(iso7064.isValid("89001567010203040506070809101152"));
     Assert.assertTrue(iso7064.isValid("89044011112233441122334411223321"));
-    Assert.assertTrue(iso7064.isValid("89001039240040102300000000149266"));
-    Assert.assertFalse(iso7064.isValid("89001039240040102300000000149265"));
-    Assert.assertTrue(iso7064.isValid("89001039240040102300000000151303"));
   }
 
   public void testIsValidIban() throws Exception {
-    String iban = "NL02ABNA0123456789";
-    String reformattedCode = iban.substring(4) + iban.substring(0, 4);
-    Assert.assertTrue(iso7064.isValid(reformattedCode));
+    // https://www.swift.com/sites/default/files/resources/iban_registry.pdf
+    Assert.assertTrue(iso7064.isValid(reformatIban("AL47212110090000000235698741")));
+    Assert.assertTrue(iso7064.isValid(reformatIban("AD1200012030200359100100")));
+    Assert.assertTrue(iso7064.isValid(reformatIban("AT611904300234573201")));
+    Assert.assertTrue(iso7064.isValid(reformatIban("AZ21NABZ00000000137010001944")));
+    Assert.assertTrue(iso7064.isValid(reformatIban("BH67BMAG00001299123456")));
+    Assert.assertTrue(iso7064.isValid(reformatIban("BE68539007547034")));
+    Assert.assertTrue(iso7064.isValid(reformatIban("BA391290079401028494")));
+    Assert.assertTrue(iso7064.isValid(reformatIban("BR9700360305000010009795493P1")));
+    Assert.assertTrue(iso7064.isValid(reformatIban("BR1800000000141455123924100C2")));
+    Assert.assertTrue(iso7064.isValid(reformatIban("BG80BNBG96611020345678")));
+    Assert.assertTrue(iso7064.isValid(reformatIban("CR0515202001026284066")));
+    Assert.assertTrue(iso7064.isValid(reformatIban("HR1210010051863000160")));
+    Assert.assertTrue(iso7064.isValid(reformatIban("CY17002001280000001200527600")));
   }
 
-  public void testCalculate() throws Exception {
+  private String reformatIban(final String iban){
+    return iban.substring(4) + iban.substring(0, 4);
+  }
+
+  public void testCalculateEid() throws Exception {
     Assert.assertEquals("24", iso7064.calculate("890010120123412340123456789012"));
     Assert.assertEquals("52", iso7064.calculate("890015670102030405060708091011"));
     Assert.assertEquals("21", iso7064.calculate("890440111122334411223344112233"));
-    Assert.assertEquals("66", iso7064.calculate("890010392400401023000000001492"));
-    Assert.assertEquals("03", iso7064.calculate("890010392400401023000000001513"));
   }
 
-  public void testCaluculateIban() throws Exception {
-    String iban = "NL00ABNA0123456789";
+  public void testCalculateIban() throws Exception {
+    String iban = "AL00212110090000000235698741";
     String reformattedCode = iban.substring(4) + iban.substring(0, 2);
-    Assert.assertEquals("02", iso7064.calculate(reformattedCode));
+    Assert.assertEquals("47", iso7064.calculate(reformattedCode));
   }
 
 }
